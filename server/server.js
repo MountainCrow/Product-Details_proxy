342dcaf0463a925a0ad793d09429c6c445b7aa06
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2020-07-23 11:40:09 -0700

    cleaned code added comments

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-//require('newrelic');
+require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -14,16 +14,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static(path.join(__dirname, '../dist')))
-
-const productDetailsLB = createProxyMiddleware('http://ec2-54-183-193-8.us-west-1.compute.amazonaws.com');
-const reviewsLB = createProxyMiddleware('http://ec2-52-53-212-33.us-west-1.compute.amazonaws.com/');
-const productServiceLB = createProxyMiddleware('http://ec2-13-57-221-22.us-west-1.compute.amazonaws.com/');
-
-
-app.use(productDetailsLB)
-  .use(productServiceLB)
-  .use(reviewsLB)
-
 
 app.get('/', (req, res) => {
   res.send('Working...')
